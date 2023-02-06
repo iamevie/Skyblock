dc1a800b64968fffbe1649838554ed1a90becc50
Leon Kalliardos <lkalliardos@gmail.com>
2023-02-06 12:30:05 +0100

    Initial commit

---
+++ src/main/java/de/ekalliardos/skyblock_mod/SkyblockMod.java
@@ -0,0 +1,41 @@
+package de.ekalliardos.skyblock_mod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(SkyblockMod.MOD_ID)
+public class SkyblockMod
+{
+    public static final String MOD_ID = "skyblock_mod";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public SkyblockMod() {
+
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+
+        }
+    }
+}
